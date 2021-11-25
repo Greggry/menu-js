2638583b611b78a7ede3521a5669930edf187581
greggry <greggryw@protonmail.com>
2021-11-25 21:25:02 +0100

    bugfix for '' passed as an option

--- menu.js
+++ menu.js
@@ -21,23 +21,24 @@ class Menu {
   askForOptions = async (depth = 0) => {
     if (depth >= 3) throw new Error('Max depth (3) exceeded in suboptions');
 
-    const promptText = `Option ${depth > 0 ? '(leave empty to go back)' : ''}: `;
+    const promptText = `Option${depth > 0 ? ' (leave empty to go back)' : ''}: `;
 
     this.logOptions();
     console.log('0: Exit');
-    let chosen = await ask.prompt(promptText);
-
-    if (+chosen === 0) return this.exit(); // stop asking for input
+    let chosen;
 
     while (!this.isInputValid(+chosen)) {
+      chosen = await ask.prompt(promptText);
+
       if (`${chosen}`.trim() === '' && depth > 0) {
         // go back
         this.activeScreen = this.activeScreen.parent;
         return this.askForOptions(--depth);
       }
 
+      if (chosen?.trim() === '0') return this.exit(); // stop asking for input
+
       console.log(`Invalid Option: "${chosen}"`);
-      chosen = await ask.prompt(promptText);
     }
 
     const optionObj = this.activeScreen[chosen - 1];
