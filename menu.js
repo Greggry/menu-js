1ba6f4e60a711e958cd9c64a915cfc2bb8fbc654
greggry <greggryw@protonmail.com>
2021-11-26 19:58:02 +0100

    implemented the onEmpty option

--- menu.js
+++ menu.js
@@ -9,13 +9,25 @@ class Menu {
       this.options.push(option);
     });
 
+    // ensure that only the last option in the array holds onEmpty=true
+    this.onEmptyOptions = this.options.filter(opt => opt.onEmpty);
+    if (this.onEmptyOptions.length > 1)
+      throw new Error('The onEmpty property option has to be the last option');
+    this.hasOnEmpty = this.onEmptyOptions.length === 1 ? true : false;
+
     this.askForOptions();
   }
 
   logOptions = (screen = this.activeScreen) => {
     screen.forEach((option, i) => {
-      console.log(`${i + 1}: ${option.text}`);
+      if (option.onEmpty) return;
+      if (!option.onEmpty) console.log(`${i + 1}: ${option.text}`);
     });
+
+    console.log('0: Exit');
+
+    // print onEmpty
+    if (this.hasOnEmpty) console.log(this.options[this.options.length - 1].onEmptyDescription);
   };
 
   askForOptions = async (depth = 0) => {
@@ -24,21 +36,24 @@ class Menu {
     const promptText = `Option${depth > 0 ? ' (leave empty to go back)' : ''}: `;
 
     this.logOptions();
-    console.log('0: Exit');
     let chosen;
 
     while (!this.isInputValid(+chosen)) {
-      chosen = await ask.prompt(promptText);
+      chosen = (await ask.prompt(promptText)).trim();
+      if (chosen === '0') return this.exit(); // stop asking for input
 
-      if (`${chosen}`.trim() === '' && depth > 0) {
-        // go back
-        this.activeScreen = this.activeScreen.parent;
-        return this.askForOptions(--depth);
+      if (chosen === '') {
+        if (depth === 0 && this.hasOnEmpty)
+          return this.runCallbackAndContinue(this.getLastOption());
+
+        // going back
+        if (depth > 0) {
+          this.activeScreen = this.activeScreen.parent;
+          return this.askForOptions(--depth);
+        }
+
+        console.log(`Invalid Option: "${chosen}"`);
       }
-
-      if (chosen?.trim() === '0') return this.exit(); // stop asking for input
-
-      console.log(`Invalid Option: "${chosen}"`);
     }
 
     const optionObj = this.activeScreen[chosen - 1];
@@ -54,6 +69,10 @@ class Menu {
       return this.askForOptions(++depth);
     }
 
+    this.runCallbackAndContinue(optionObj);
+  };
+
+  runCallbackAndContinue = optionObj => {
     optionObj.callbackFn();
     if (optionObj.doExitOnCallback) return this.exit();
 
@@ -62,6 +81,7 @@ class Menu {
   };
 
   isInputValid = input => input > 0 && input <= this.activeScreen.length;
+  getLastOption = () => this.options[this.options.length - 1];
   exit = () => ask.closePrompt();
 }
 
